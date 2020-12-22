9d97492999f06cf7f1995e5190acdb3601a5c87a
SergeyGolenko <1989sergiotay@gmail.com>
2020-12-22 19:30:26 +0200

    connected the outlets and action to viewController

--- WhatWiilWeather/ViewController.swift
+++ WhatWiilWeather/ViewController.swift
@@ -8,7 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
+    @IBOutlet weak var cityTextField: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    
+    @IBAction func getWeather(_ sender: Any) {
+    }
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
